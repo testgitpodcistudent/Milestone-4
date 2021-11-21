a1cb4c24e19fe57ed429b15caf53e8e9704e7872
RoryBr1 <rorybr1@gmail.com>
2021-11-21 21:52:44 +0000

    created URL path for /blog/edit and directed to edit_post view in blog/views.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.view_blog, name="view_blog"),
     path("create_post/", views.create_post, name="create_post"),
     path("delete_post/<int:post_id>/", views.delete_post, name="delete_post"),
+    path("edit/<int:post_id>/", views.edit_post, name="edit_post"),
 ]
