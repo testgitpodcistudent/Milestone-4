3925f126de4c317b62628470576f3ad6f3192350
RoryBr1 <rorybr1@gmail.com>
2021-11-22 17:40:46 +0000

    added a newline to products/urls.py for PEP8 compliancy. Split lines in AUTH_PASSWORD_VALIDATORS in techzone/settings.py for PEP8 compliancy.

--- techzone/settings.py
+++ techzone/settings.py
@@ -144,16 +144,20 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': ('django.contrib.auth.password_vali'
+                 'dation.UserAttributeSimilarityValidator',)
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': ('django.contrib.auth.password_vali'
+                 'dation.MinimumLengthValidator',)
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': ('django.contrib.auth.password_vali'
+                 'dation.CommonPasswordValidator',)
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': ('django.contrib.auth.password_validatio'
+                 'n.NumericPasswordValidator',)
     },
 ]
 
