47114607782bd83b179f242e0e4798d3e943de2e
testgitpodcistudent <raxoxob374@keagenan.com>
2021-11-22 21:12:42 +0000

    typo in settings.py

--- techzone/settings.py
+++ techzone/settings.py
@@ -218,7 +218,7 @@ else:
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-    EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASS']
+    EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
     DEFAULT_FROM_EMAIL = os.environ['EMAIL_HOST_USER']
 
 
