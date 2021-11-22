f5a3bf30b61e73b0e5408b99f3b148bc20a0d818
RoryBr1 <rorybr1@gmail.com>
2021-11-22 13:03:38 +0000

    blog/models.py: max_length of 250 added to comment field. __str__(self) now returns the username, a snippet of the comment, and the date of comment. | blog/widgets.py: line unbroken which was returning an error.

--- blog/models.py
+++ blog/models.py
@@ -25,7 +25,7 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
-    content = models.TextField()
+    content = models.TextField(max_length=250)
 
     def __str__(self):
-        return self.user.username
+        return self.user.username + ': "' + self.content[0:35] + '" (' + str(self.date) + ')'
