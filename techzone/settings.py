be36f53bc4cae39679631537f69a703d92903b04
RoryBr1 <rorybr1@gmail.com>
2021-11-22 17:03:11 +0000

    Added subtext with blog link to index.html. Changed DEFAULT_FROM_EMAIL in settings.py.

--- techzone/settings.py
+++ techzone/settings.py
@@ -211,7 +211,7 @@ STRIPE_WH_SECRET = os.environ["STRIPE_WH_SECRET"]
 # Email
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'techzone@example.com'
+    DEFAULT_FROM_EMAIL = 'techzone@techzone.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
